09778ab15ea7b63a8be6d06955257c82d67ccae9
Peter Krieg <peter.krieg@ert.com>
2018-02-15 10:08:18 -0500

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-unused-vars': ['error', {
+      vars: 'local',
+      args: 'after-used',
+      argsIgnorePattern: '^_$'
+    }],
+    indent: [1, 'tab', {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      MemberExpression: 0,
+      // CallExpression: {
+        // parameters: null,
+      // },
+      FunctionDeclaration: {
+        parameters: 1,
+        body: 1
+      },
+      FunctionExpression: {
+        parameters: 1,
+        body: 1
+      }
+    }],
+    'no-tabs': 0,
+
+
+
+  }
+}
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const app = require('./express');
 
-const { PORT: port = 3000 } = process.env
+const { PORT: port = 3000 } = process.env;
+
+function test() {
+	const x = 2;
+}
 
 
 app.listen(port, _ => {
-  console.log(`listening on port ${port}`);
+	console.log(`listening on port ${port}`);
 
 })
